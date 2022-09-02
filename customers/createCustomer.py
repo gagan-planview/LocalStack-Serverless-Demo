4e2a0e7a6d193615b087d9b8336eb6fc1476ed0f
Sourabh Prasanna Kumar <sprasanna@planview.com>
2022-09-02 11:43:02 +0530

    Send email from create customer.

--- customers/createCustomer.py
+++ customers/createCustomer.py
@@ -11,10 +11,11 @@ from boto3 import session
 if 'LOCALSTACK_HOSTNAME' in os.environ:
     dynamodb_endpoint = f"http://{os.environ['LOCALSTACK_HOSTNAME']}:4566"
     dynamodb = boto3.resource('dynamodb', endpoint_url=dynamodb_endpoint)
+    ses_endpoint = f"http://{os.environ['LOCALSTACK_HOSTNAME']}:4566"
+    ses_client = boto3.client("ses", region_name="us-west-2", endpoint_url=ses_endpoint)
 else:
     dynamodb = boto3.resource('dynamodb')
-
-lambda_client = boto3.client('lambda')
+    ses_client = boto3.client('ses')
 
 def createCustomer(event, context):
 
@@ -37,5 +38,35 @@ def createCustomer(event, context):
 
     # write the Customer to the database
     table.put_item(Item=customer)
+
+    if 'email' not in data:
+        logging.error("Validation Error")
+        raise Exception("Email not found.")
+
+    else:
+        destinationEmail = data['email']
+        response = ses_client.send_email(
+            Source='hackathonSourceEmail@planview.com',
+            Destination={
+                'ToAddresses': [destinationEmail],
+            },
+            ReplyToAddresses=['hackathonSourceEmail@planview.com'],
+            Message={
+                'Subject': {
+                    'Data': 'New customer created',
+                    'Charset': 'utf-8'
+                },
+                'Body': {
+                    'Text': {
+                        'Data': 'New customer created',
+                        'Charset': 'utf-8'
+                    },
+                    'Html': {
+                        'Data': 'New customer created',
+                        'Charset': 'utf-8'
+                    }
+                }
+            }
+        )
 
     return {"statusCode": 200, "body": json.dumps(customer)}
